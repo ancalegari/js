207b277a6b318be417c28797c9a961fc99ed806e
ancalegari <160127066+ancalegari@users.noreply.github.com>
2024-04-24 08:56:34 -0300

    Update cadastra.js

--- cadastra.js
+++ cadastra.js
@@ -1,8 +1,11 @@
 function limpar(){
-  document
+  const campos = document
     .queryselectorall("input .is-error, textarea .is-error")
-    foreach(()=>{console.log("campo com erro")})
-    .classList
-    .remove("is-error")
+  console.log(campos)
+  campos
+    .foreach((input)=>{input.classlist.remove("is-error")})
+
+  document.querySelectorAll("nes-field span")
+    .forEach(span => span.innerText = "")
 
 }
